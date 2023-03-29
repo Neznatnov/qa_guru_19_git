6468b69b1f880fbc6a1639f99ac92b158a3c3df0
Nika <83816253+Neznatnov@users.noreply.github.com>
2023-03-29 20:41:21 +0200

    Update Main.java

--- src/main/java/com/neznatnov/Main.java
+++ src/main/java/com/neznatnov/Main.java
@@ -2,6 +2,6 @@ package com.neznatnov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nika is Great!");
+        System.out.println("Hello world!");
     }
 }
